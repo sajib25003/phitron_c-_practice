a0417c875cd4ed96f76dcdfd4c15acfec8f13cf2
Md. Ashik Hassan Bhuiyan <sjb.25003@gmail.com>
2025-11-18 09:11:29 +0600

    first commit

---
+++ Module-2/2.2_dynamic_variable.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int *p = new int;
+    *p = 199;
+    cout << *p << endl;
+    return 0;
+}
---
+++ Module_1.1/print.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    // std::cout << "Hello World from Print!";
+    // int x=10;
+    // std::cout << "The value of x is: " << std::endl << x;
+
+    int x;
+    // taking input value
+    // std::cin >> x;
+    cin >> x;
+    // std::cout << x << std::endl;
+    cout << x << endl;
+
+    char c = 'a';
+    int ascii = c;
+    cout << ascii << endl;
+    cout << (int)c << endl;
+    return 0;
+}
